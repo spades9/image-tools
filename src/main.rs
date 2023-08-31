ebf2ce0ed61280a81e3c43c91e548de6156558c8
15716862191@qq.com <7zsx6CmJz592pcqi>
2023-08-31 18:31:27 +0800

    修改

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![windows_subsystem = "windows"]
+//#![windows_subsystem = "windows"]
 use std::{time::Duration, rc::Rc, thread};
 use slint::{CloseRequestResponse, SharedString, ModelRc, VecModel, Model};
 slint::include_modules!();
@@ -35,22 +35,17 @@ async fn main() {
             });
         }
     });
-
-    win.on_clicked(|_e:i32|{
-        
-    });
     
     let compress = win.as_weak().unwrap();
-    //获取目录
-    win.on_compress_open(move ||{
+    //Get directory
+    win.global::<CompressGlobal>().on_open_folder(move ||{
         info!("select folder");
         let path = open_folder();
         if !path.eq("") {
-            //输出目录到页面
+            //Output to page
             compress.global::<CompressGlobal>().set_compress_path(SharedString::from(&path));
-            let image_list = compress.global::<CompressGlobal>().get_image_list();
             let mut new_image_list = Vec::new();
-            //获取到这个目录之后获取这个文件夹下的所有图片文件
+            //Get all files under this folder
             let read_result = read_dir(&path);
             if let Ok(dirs) = read_result {
                 for entiy in dirs {
@@ -59,7 +54,6 @@ async fn main() {
                         if let Ok(f) = metadata(&path){
                             if f.is_file() {
                                 if let Some(file_name) = path.file_name(){
-                                    //info!("file_name:{}",file_name.to_str().unwrap());
                                     new_image_list.push(ImageList{
                                         name:SharedString::from(file_name.to_str().unwrap()),
                                         path:SharedString::from(file_name.to_str().unwrap()),
@@ -87,9 +81,17 @@ async fn main() {
         let image_list = compress.global::<CompressGlobal>().get_image_list();
         let compress = compress.as_weak();
         let mut list = Vec::new();
-        let count = image_list.row_count();
+        let mut count:i32 = 0;
+        let temp = image_list.row_data(0);
+        println!("temp:{:?}",temp);
+        while image_list.row_data(count as usize).is_some() {
+            info!("count:{}",count);
+            count = count + 1;
+        }
+        
+        info!("get count:{}",count);
         for i in 0..count {
-            if let Some(data) = image_list.row_data(i){
+            if let Some(data) = image_list.row_data(i as usize){
                 list.push(
                     ImageListData{
                         name:data.name.to_string(),
@@ -102,15 +104,16 @@ async fn main() {
         move || {
             let win_weak = compress.clone();
             tokio::spawn(async move {
-                for li in 0..6{
+                info!("count:{}",count);
+                for li in 0..count{
                     info!("开始处理");
                     let _ = win_weak.clone().upgrade_in_event_loop(move |hello|{
                         info!("开始更新页面");
                         let image_list = hello.global::<CompressGlobal>().get_image_list();
-                        let mut d = image_list.row_data(li).unwrap();
+                        let mut d = image_list.row_data(li as usize).unwrap();
                         d.status = 1;
                         info!("改变状态");
-                        image_list.set_row_data(li, d);
+                        image_list.set_row_data(li as usize, d);
                         hello.window().request_redraw();
                     });
                     thread::sleep(Duration::from_millis(2000));
@@ -119,15 +122,14 @@ async fn main() {
                     let _ = win_weak.clone().upgrade_in_event_loop(move |hello|{
                         info!("开始更新页面");
                         let image_list = hello.global::<CompressGlobal>().get_image_list();
-                        let mut d = image_list.row_data(li).unwrap();
+                        let mut d = image_list.row_data(li as usize).unwrap();
                         d.status = 2;
                         info!("改变状态");
-                        image_list.set_row_data(li, d);
+                        image_list.set_row_data(li as usize, d);
                         hello.window().request_redraw();
                     });
                 }
             });
-            
         }
         
 
